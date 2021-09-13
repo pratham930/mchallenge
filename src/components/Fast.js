3f0ea45885211d2308ae45126c2effc7de459154
pratham930 <85028976+pratham930@users.noreply.github.com>
2021-09-13 15:50:45 +0530

    media query on fast.js page

--- src/components/Fast.js
+++ src/components/Fast.js
@@ -5,21 +5,28 @@ import "./cart.css";
 const Fast = ({ id, name, price, quantity }) => {
   const { removeItem, increment, decrement } = useContext(MartContext);
   return (
-     <>
-    <div className="grid-container">
-       
-    <div className=""><h5> {name}</h5></div>
-       
-         <div > {quantity} </div>
-    
-   
-    
-       <div className='bnt' >  <button className='btn1' onClick = {()=>increment(id)}>+</button>
-         <button className='btn2' onClick = {()=>decrement(id)}>-</button>
-         <button className='btn3' onClick = {()=>removeItem(id)}>delete</button></div>
-     
-     </div>
-     </>
+    <>
+      <div className="grid-container" id="cont">
+        <div className="">
+          <h5> {name}</h5>
+        </div>
+
+        <div> {quantity} </div>
+
+        <div className="bnt">
+          {" "}
+          <button className="btn1" onClick={() => increment(id)}>
+            +
+          </button>
+          <button className="btn2" onClick={() => decrement(id)}>
+            -
+          </button>
+          <button className="btn3" onClick={() => removeItem(id)}>
+            delete
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 export default Fast;
